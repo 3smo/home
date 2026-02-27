6eddf469f0cea19037e1c8244c9a2856816ed4a1
3smo <3smartodisha@gmail.com>
2026-02-27 12:06:55 +0530

    Update vfgorder.php

--- vfgorder.php
+++ vfgorder.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
     <?php include 'header.html'; ?>
-
-    <h1>Welcome to my home page!</h1>
-    <p>Some text.</p>
-
+    <div id="home" class="tab-content active">
+        <div >
+		<iframe 
+				src="https://forms.gle/XJ3icMzhqc9wsc3G6" 
+				style="border:none; width:100%; height:1400px;margin-top:100px;" 
+				title="3S Mart Odisha Food Order">
+			</iframe>
+   
+        </div>
+    </div>
     <?php include 'footer.html'; ?>
 </html>
