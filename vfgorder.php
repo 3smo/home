619782d0fdaabc83b3fefea1e8b72673ba32b861
3smo <3smartodisha@gmail.com>
2026-02-27 11:58:40 +0530

    Create vfgorder.php

---
+++ vfgorder.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php include 'header.html'; ?>
+
+    <h1>Welcome to my home page!</h1>
+    <p>Some text.</p>
+
+    <?php include 'footer.html'; ?>
+
+</body>
+</html>
