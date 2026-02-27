64cf65e1d256d47cc57f615d2af801899a6b3ed6
3smo <3smartodisha@gmail.com>
2026-02-27 12:06:03 +0530

    Update vfgorder.php

--- vfgorder.php
+++ vfgorder.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html>
-<body>
-
     <?php include 'header.html'; ?>
 
     <h1>Welcome to my home page!</h1>
     <p>Some text.</p>
 
     <?php include 'footer.html'; ?>
-
-</body>
 </html>
